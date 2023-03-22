d038d057679e4603cb8e2cce43955ef86106089f
Hardeep <hardeep.singh@lpinfotech.com>
2023-03-22 11:18:57 +0530

    update sidebar.component

--- ClientApp/src/app/shared/components/sidebar/sidebar.component.ts
+++ ClientApp/src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { NgbOffcanvas, OffcanvasDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import {
+  NgbOffcanvas,
+  OffcanvasDismissReasons,
+} from '@ng-bootstrap/ng-bootstrap';
+import { CommonService } from '../../services/common.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,32 +12,31 @@ import { NgbOffcanvas, OffcanvasDismissReasons } from '@ng-bootstrap/ng-bootstra
 })
 export class SidebarComponent implements OnInit {
   closeResult = '';
-  
-  constructor(private offcanvasService: NgbOffcanvas) {}
-  ngOnInit() {} /* TODO document why this method 'ngOnInit' is empty */ 
-  
-  open(content) {
-		this.offcanvasService.open(content, { ariaLabelledBy: 'offcanvas-basic-title' }).result.then(
-			(result) => {
-				this.closeResult = `Closed with: ${result}`;
-			},
-			(reason) => {
-				this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-			},
-		);
-	}
+  constructor(
+    private offcanvasService: NgbOffcanvas,
+    private commonService: CommonService
+  ) {}
+  ngOnInit() {}
+    open(content) {
+    this.offcanvasService
+      .open(content, { ariaLabelledBy: 'offcanvas-basic-title' })
+      .result.then(
+        (result) => {
+          this.closeResult = `Closed with: ${result}`;
+        },
+        (reason) => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        }
+      );
+  }
 
-	private getDismissReason(reason: any): string {
-		if (reason === OffcanvasDismissReasons.ESC) {
-			return 'by pressing ESC';
-		} else if (reason === OffcanvasDismissReasons.BACKDROP_CLICK) {
-			return 'by clicking on the backdrop';
-		} else {
-			return `with: ${reason}`;
-		}
-	}
-
-
-
-
+  private getDismissReason(reason: any): string {
+    if (reason === OffcanvasDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === OffcanvasDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on the backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
 }
